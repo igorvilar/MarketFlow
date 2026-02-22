10b7ec8aaad28765605ead4fc40aff3fd8bbed5f
Igor Carvalho Vilar <igorvilar@outlook.com>
2026-02-22 11:01:08 -0300

    criado projeto no xcode

---
+++ MarketFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarketFlow
+//
+//  Created by Igor Vilar on 22/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MarketFlowTests/MarketFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarketFlowTests.swift
+//  MarketFlowTests
+//
+//  Created by Igor Vilar on 22/02/26.
+//
+
+import Testing
+@testable import MarketFlow
+
+struct MarketFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
